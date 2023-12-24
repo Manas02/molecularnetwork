14544b454a86fb31d6e6d2736245da8967bb0c4c
Manas Mahale <manas.mahale2002@gmail.com>
2023-12-24 15:18:35 +0530

    description added

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
 setup(
     name="molecularnetwork",
     version="0.2.0",
@@ -14,5 +18,7 @@ setup(
     author="Manas Mahale",
     author_email="manas.m.mahale@gmail.com",
     description="A package for creating molecular networks based on molecular features and similarities.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/Manas02/molecularnetwork",
 )
