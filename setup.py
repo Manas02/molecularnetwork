67b75bed13b4677022664a162e6d4725efe3ccdb
Manas Mahale <manas.mahale2002@gmail.com>
2024-04-07 22:27:32 +0530

    version change

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="molecularnetwork",
-    version="0.3.5",
+    version="0.3.6",
     packages=find_packages(),
     install_requires=[
         "numpy",
