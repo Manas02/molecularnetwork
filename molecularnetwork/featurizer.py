535f8276386697dbdb6679acceca891bbfb4e121
Manas Mahale <manas.mahale2002@gmail.com>
2024-04-13 00:55:16 +0530

    update to features

--- molecularnetwork/featurizer.py
+++ molecularnetwork/featurizer.py
@@ -12,12 +12,10 @@ class FingerprintCalculator:
     def __init__(self, descriptor="morgan2"):
         self.descriptor = descriptor
         self.descriptors = {
-            "atompairs": lambda m: Pairs.GetAtomPairFingerprint(m),
             "maccs": lambda m: MACCSkeys.GenMACCSKeys(m),
             "morgan2": lambda m: AllChem.GetMorganFingerprintAsBitVect(m, 2, 2048),
             "morgan3": lambda m: AllChem.GetMorganFingerprintAsBitVect(m, 3, 2048),
             "rdkit": lambda m: FingerprintMols.FingerprintMol(m),
-            "topo": lambda m: Torsions.GetTopologicalTorsionFingerprint(m),
         }
 
     def calculate_fingerprint(self, smi):
